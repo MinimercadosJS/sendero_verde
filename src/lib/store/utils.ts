025adac5c230962c243384005d9ece7958cb88ef
sebasRT <rtsebas11@gmail.com>
2025-04-17 16:54:08 -0400

    fix: update store hours in isStoreOpen function for accurate opening times

--- src/lib/store/utils.ts
+++ src/lib/store/utils.ts
@@ -3,5 +3,5 @@ import { DateTime } from 'luxon';
 export function isStoreOpen(): boolean {
   const currentHour = DateTime.now().setZone('America/Bogota').hour;
 
-  return currentHour >= 7 && currentHour < 21;
+  return currentHour >= 12 && currentHour < 20;
 }
